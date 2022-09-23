6dbde839a07efde140f466c3abe564b2eb000a22
atish9937 <atish9937@gmail.com>
2022-09-23 20:01:29 +0530

    Update flaskdemo.py

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,22 +1,16 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, redirect, request
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-	return 'Hello World'
+from urllib.parse import urlparse, urlunparse
 
-# main driver function
-if __name__ == '__main__':
+FROM_DOMAIN = "example.com"
+TO_DOMAIN = "www.example.com"
 
-	# run() method of Flask class runs the application
-	# on the local development server.
-	app.run()
+@app.before_request
+def redirect_to_new_domain():
+    urlparts = urlparse(request.url)
+    if urlparts.netloc == FROM_DOMAIN:
+        urlparts_list = list(urlparts)
+        urlparts_list[1] = TO_DOMAIN
+        return redirect(urlunparse(urlparts_list), code=301)
